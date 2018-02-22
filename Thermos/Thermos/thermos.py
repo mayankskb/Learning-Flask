633cce81e6e813da07b7f4c4db7c9d4a0a34806f
mayankskb <mayank1996.skb@hotmail.com>
2018-02-22 16:51:28 +0530

    Thermos commit -II

--- Thermos/Thermos/thermos.py
+++ Thermos/Thermos/thermos.py
@@ -2,10 +2,25 @@ from flask import Flask, render_template, url_for
 
 app  = Flask(__name__)
 
+class User:
+    def __init__(self, firstname, lastname, occupation):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.occupation = occupation
+
+    def Firstname(self):
+        return "First Name : {} ".format(self.firstname)
+
+    def Lastname(self):
+        return "Last Name : {}".format(self.lastname)
+
+    def Occupation(self):
+        return "Occupation : {}".format(self.occupation)
+
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', title = "Author Introduction", user = User('Mayank', 'Mishra', 'Data Science Engineer'))
 
 
 if __name__ == "__main__":
